4c5ff9fefe46527895dc7df8fde31b97eb5c21e1
Popoola_Sinaayo <olusegunpopoola4real@gmail.com>
2025-10-12 14:46:39 +0100

    update on code

--- App.tsx
+++ App.tsx
@@ -4,13 +4,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import BaseNavigator from "./navigator/baseNavigator";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import FlashMessage from "react-native-flash-message";
-import {
-  StatusBar as ExpoStatusBar,
-  setStatusBarBackgroundColor,
-} from "expo-status-bar";
-import { useEffect } from "react";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+      shouldShowBanner: true,
+      shouldShowList: true,
+    }),
+  });
   const queryClient = new QueryClient();
   return (
     <NavigationContainer>
